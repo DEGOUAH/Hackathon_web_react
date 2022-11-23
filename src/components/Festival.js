847ff74c58a4a857b396cb634b592fa9fee55e7a
raouf <abderraouf.degouah@gmail.com>
2022-11-23 23:31:34 +0100

    font modification

--- src/components/Festival.js
+++ src/components/Festival.js
@@ -3,15 +3,18 @@ import festival from './../festival.png';
 import "./Festival.css";
 
 const Festival = ({ name, discipline, commune  }) => {
+    const onclick = () => {
+
+    }
     return (
-        <div className='card'>
+        <div className='card' onClick={onclick}>
             <div className='card_image'>
                 <img src={festival}  alt="festival image"  />
             </div>
             <div className='card_info'>
                 <h2>{name}</h2>
-                <h3>{discipline}</h3>
-                <h3>{commune}</h3>
+                <h3><span className="description">Genre : </span>{discipline}</h3>
+                <h3><span className="description">Ville : </span>{commune}</h3>
                 {/* <h2>{title}</h2> */}
             </div>
         </div>
